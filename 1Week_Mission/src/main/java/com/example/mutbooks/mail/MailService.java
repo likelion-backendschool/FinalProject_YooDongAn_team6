543560e32353ae3af3008a64886c593a31654131
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-10-19 12:48:11 +0900

    설정 변경 및 부트스트랩 추가

--- 1Week_Mission/src/main/java/com/example/mutbooks/mail/MailService.java
+++ 1Week_Mission/src/main/java/com/example/mutbooks/mail/MailService.java
@@ -1,6 +1,7 @@
 package com.example.mutbooks.mail;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -10,9 +11,12 @@ import org.springframework.stereotype.Service;
 public class MailService {
     private final JavaMailSender javaMailSender;
 
+    @Value("${spring.mail.username}")
+    private String username;
+
     public void sendMail(String email) {
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-        simpleMailMessage.setFrom("dbehddks519@naver.com");
+        simpleMailMessage.setFrom(username);
         simpleMailMessage.setTo(email);
         simpleMailMessage.setSubject("회원가입을 축하드립니다!");
         simpleMailMessage.setText("회원님의 멋북스 회원가입을 축하드립니다! 항상 더 나은 서비스로 보답하겠습니다. 감사합니다.");
