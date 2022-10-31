91d22dc3be546077ef758ab365ce8cfcb13eb774
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-10-31 22:41:30 +0900

    setting

---
+++ 3Week_Mission/src/main/java/com/ll/exam/final__2022_10_08/Final20221008Application.java
@@ -0,0 +1,25 @@
+package com.ll.exam.final__2022_10_08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootApplication
+@EnableJpaAuditing
+@EnableAsync
+public class Final20221008Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Final20221008Application.class, args);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
